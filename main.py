e02e191174e420178c5695df8e4c6620e124969d
mateuspatz <mateus_patz@hotmail.com>
2021-11-13 18:27:55 -0300

    Adiciona leitura do Subject Key Ident. do Root

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import argparse
 import os
-import mimetypes
-from typing import NewType
 from cryptography import x509
-# from cryptography.hazmat.backends import default_backend
-# import cryptography
 
 parser = argparse.ArgumentParser(description="cert-checker - Check if a certificate is trusted, based on a list of trusted root CA")
 parser.add_argument('--certificate', action='store', type=str, required=True)
@@ -13,6 +9,8 @@ args=parser.parse_args()
 
 certificateToCheckFileName=args.certificate
 trustedRootFileName=args.trustedRootCert
+
+subjectKeyIdentifier=x509.ObjectIdentifier("2.5.29.14")
 
 ########### READ CERTIFICATE ###########
 
@@ -56,19 +54,27 @@ except:
     exit(1)
 
 # Try to parse certificate file
-# try:
-trustedRoot = x509.load_der_x509_certificate(trustedRootCertData,backend=None)
-# except Exception as e:
-#     print(e)
-#     print("Erro ao processar o certificado root")
-#     exit(1)
+try:
+    trustedRoot = x509.load_der_x509_certificate(trustedRootCertData,backend=None)
+except Exception as e:
+    print(e)
+    print("Erro ao processar o certificado root")
+    exit(1)
 
-#### Ler data de validade dos certificados ####
-
-
+#### Read date time of certificates ####
 
 
 
 
-#### Obter Subject Key do Root
 
+
+#### Get Subject Key of Root
+
+# for extension in trustedRoot.extensions:
+#     print(extension)
+
+# print(trustedRoot.extensions.get_extension_for_oid(subjectKeyIdentifier))
+trustedRootSubjectKeyIdentifier=trustedRoot.extensions.get_extension_for_oid(subjectKeyIdentifier).value.digest
+print("trusted Root Subject Key Identifier:\n\t OID: [",subjectKeyIdentifier.dotted_string,"], Hex Value:",trustedRootSubjectKeyIdentifier.hex())
+
+#### Get Issuer of 
